120342a74bff9f42362886aba1ed7c7b7f496790
Carys <carysb17@gmail.com>
2018-11-27 22:39:11 +0000

    Added footer with JQuery timer and human traffic count. Reformatted content on index.html. Ammended social buttons and form on about_us.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+/* Scroll */
 $(window).on('scroll', function() {
   var scrollAmount = window.scrollY
   if (scrollAmount > 50) {
     $(".arrow").animate({ opacity: 0});
   }
 })
+
+/* Traffic Count setInterval */
+var trafficCountIncrement = 0;
+var trafficCountElement = document.getElementById('traffic-count');
+
+setInterval(increaseTrafficCount, 40000);
+
+function increaseTrafficCount() {
+  trafficCountIncrement++;
+  if (trafficCountIncrement == 1) {
+    trafficCountElement.innerText = trafficCountIncrement + ' Person trafficked since you visited this site';
+  }
+  else {
+    trafficCountElement.innerText = trafficCountIncrement + ' People trafficked since you visited this site';
+  }
+}
+
+/* Timer */
+var timerIncrement = 0;
+var timerElement = document.getElementById('timer');
+
+setInterval(increaseTimer, 1000);
+
+function increaseTimer() {
+  timerIncrement++;
+  timerElement.innerText = timerIncrement + 's';
+}
